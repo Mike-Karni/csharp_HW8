2f0d61593bc63c7e325b4702c7d12d382fba5de2
mike_karni <3923499@mail.ru>
2022-05-19 19:31:11 +0300

    Task61Done

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/**Задача 62:**
+
+Заполните спирально массив 4 на 4.*/
+
+int[,] snakeArray = new int[4,4];
+
+
+
+for(int i = 0; i < 1; i++)
+{
+    for(int j = 1; j < snakeArray.Length; j++)
+    {
+        snakeArray[0,0] = 1;
+        snakeArray[0,j] = snakeArray[0,j] + 1;
+    }
+}
+
+for(int i = 0; i < snakeArray.Length; i++)
+{
+    for(int j = 0; j < snakeArray.Length; j++)
+    {
+        Console.Write(snakeArray[0,j]);
+    }
+    Console.WriteLine();
+}
--- task7/Program.cs
+++ task7/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Task61 Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника
+
+Console.Write("Size of Matrix: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[,] pascal = new int[size, size];
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        if (i == j || j == 0)
+        {
+            pascal[i, j] = 1;
+        }
+        else if (j > i)
+        {
+            pascal[i, j] = 0;
+        }
+        else if (i != j)
+        {
+            pascal[i, j] = pascal[i - 1, j - 1] + pascal[i - 1, j];
+        }
+        
+        Console.Write($"{pascal[i, j],5 }");
+    }
+    Console.WriteLine();
+}
