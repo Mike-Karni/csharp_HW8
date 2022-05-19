63676977c9569a04c4ca1f8a77c9deb7eac8f13f
mike_karni <3923499@mail.ru>
2022-05-19 09:34:04 +0300

    Task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -4,65 +4,12 @@
 Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с 
 наименьшей суммой элементов.*/
 
-/*Console.Write("Введите количество строк m: "); 
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите количество столбцов n: ");
-int n = Convert.ToInt32(Console.ReadLine());
-double[,] array = new double[m,n];
 
-FillArray(array);
-PrintArray(array);
-Console.WriteLine("Сумма значений всех элементов массива   ");
-AllSum(array,m,n);
+  
+                
+     
 
-
-
-//РЕШЕНИЕ ЗАДАЧИ- ОСТАЛЬНОЕ- ПРОВЕРКА
-
-double AllSum(double[,] array,double cows,double columns)
-{
-    double sum = 0;
-    for (int j=0; j<columns; j++)
-    {  
-        for (int i = 0; i < cows; i++)
-        {
-            sum += array[i, j];
-        }
-        
-         
-    }
-    Console.WriteLine(sum);
-    return sum;
-}
-
-//МЕТОД ПЕЧАТИ МАССИВА
-void PrintArray(double[,] matr) 
-{
-    for (int rows = 0; rows < matr.GetLength(0); rows++)
-    {
-        for (int columns = 0; columns < matr.GetLength(1); columns++)
-        {
-            Console.Write($" {matr[rows, columns]} ");   
-        }
-        Console.WriteLine();
-    }
-}
-// МЕТОД ЗАПОЛНЕНИЯ МАССИВА
-void FillArray(double[,] matr) 
-{
-    for (int rows = 0; rows < matr.GetLength(0); rows++)
-    {
-        for (int columns = 0; columns < matr.GetLength(1); columns++)
-        {
-            matr[rows,columns] = new Random().Next(1,11); 
-        }
-        Console.WriteLine();
-    }
-}
-
-*/
-
-    int n1, n2;
+int n1, n2;
                 int[,] A;
      
                 Console.Write("Введите количество строк в матрице: ");
@@ -98,4 +45,4 @@ void FillArray(double[,] matr)
      
                  Console.WriteLine("Строка с минимальной суммой элементов");
                      for(int j = 0;j<n2;j++)
-                          Console.Write(A[indexMinRow, j] + "\t");
+                          Console.Write(A[indexMinRow, j] + "\t");"\t");
