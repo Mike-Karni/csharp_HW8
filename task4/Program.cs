bac634e3ef22d2dece22b44235e01145ece6066e
mike_karni <3923499@mail.ru>
2022-05-19 09:20:39 +0300

    Task2Done

--- task4/Program.cs
+++ task4/Program.cs
@@ -4,6 +4,69 @@
 
 
 
+
+//Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.Write("Строк M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Столбцов N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] array = new int [m, n];
+
+Console.WriteLine("Начальный массив");
+PrintArray(array);
+
+for (var i = 0; i < myArray.GetLength(0); i++)
+{
+    int[] tempArray = new int[myArray.GetLength(1)];
+    for(int j = 0; j < myArray.GetLength(1); j++)
+    {
+        tempArray[j] = myArray[i, j];
+    }
+    tempArray = BubbleSort(tempArray);
+
+    for (var j = 0; j < myArray.GetLength(1); j++)
+    {
+        myArray[i, j] = tempArray[j];
+    }
+}
+
+Console.WriteLine("Конечный массив: ");
+PrintArray(myArray);
+
+
+
+void PrintArray(int[,] matr) 
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            Console.Write($" {matr[rows, columns]} ");   
+        }
+        Console.WriteLine();
+    }
+}
+
+// МЕТОД ЗАПОЛНЕНИЯ МАССИВА
+
+void FillArray(int[,] matr) 
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            matr[rows,columns] = new Random().Next(1,11); 
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+/*
 using System;
     using System.Linq;
      
@@ -45,4 +108,4 @@ using System;
                 
             }
         }
-    }
+    } */
