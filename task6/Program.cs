1734637abf97ed77458577c03840b3dab5a78bfe
mike_karni <3923499@mail.ru>
2022-05-19 20:21:31 +0300

    Task6

--- task6/Program.cs
+++ task6/Program.cs
@@ -4,22 +4,53 @@
 
 int[,] snakeArray = new int[4,4];
 
+/*1     2   3   4
 
+12   13  14   5
 
-for(int i = 0; i < 1; i++)
+11   16  15   6
+
+10    9  8    7*/
+
+    
+int main()
 {
-    for(int j = 1; j < snakeArray.Length; j++)
-    {
-        snakeArray[0,0] = 1;
-        snakeArray[0,j] = snakeArray[0,j] + 1;
-    }
-}
+    int a[10,10] = {0};
+    int N, M;
 
-for(int i = 0; i < snakeArray.Length; i++)
-{
-    for(int j = 0; j < snakeArray.Length; j++)
-    {
-        Console.Write(snakeArray[0,j]);
+    scanf("%d%d", &N, &M);
+    
+    int Ibeg = 0, Ifin = 0, Jbeg = 0, Jfin = 0;
+    
+    int k = 1;
+    int i = 0;
+    int j = 0;
+
+    while (k <= N * M){
+        a[i][j] = k;
+        if (i == Ibeg && j < M - Jfin - 1)
+            ++j;
+        else if (j == M - Jfin - 1 && i < N - Ifin - 1)
+            ++i;
+        else if (i == N - Ifin - 1 && j > Jbeg)
+            --j;
+        else
+            --i;
+
+        if ((i == Ibeg + 1) && (j == Jbeg) && (Jbeg != M - Jfin - 1)){
+            ++Ibeg;
+            ++Ifin;
+            ++Jbeg;
+            ++Jfin;
+        }
+        ++k;
     }
-    Console.WriteLine();
+    
+    for (int i = 0; i < 10; ++i){
+        for (int j = 0; j < 10; ++j)
+            printf("%3d", a[i][j]);
+        printf("\n");
+    }
+
+    return 0;
 }
